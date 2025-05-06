13cd01175fe2508ad642c8167595ff7df5a7485d
Nathan <46632454+TheNathanSpace@users.noreply.github.com>
2025-05-06 15:32:15 -0600

    Update benchmark.bash

--- myTests/benchmark.bash
+++ myTests/benchmark.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <device_id>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <device_id> <batch_size>"
     exit 1
 fi
 device_id=$1
+batch_size=$2
 
 pip install psutil
 
@@ -19,30 +20,28 @@ echo "Device name: ${device_id}"
 echo "Starting ./Deepspeech2 script in background..."
 echo "-------"
 
-for batch_size in 1 2 4 8 16 32; do
-    echo "Running with batch size: ${batch_size}"
+echo "Running with batch size: ${batch_size}"
 
-    output_file="output/${device_id}_${batch_size}_output.txt"
-    echo "" > $output_file
-    echo "Output file: ${output_file}"
+output_file="output/${device_id}_${batch_size}_output.txt"
+echo "" > $output_file
+echo "Output file: ${output_file}"
 
-    # Run the Deepspeech2 script in the background
-    python -u ./Deepspeech2 $batch_size > "${output_file}" 2>&1 &
+# Run the Deepspeech2 script in the background
+python -u ./Deepspeech2 $batch_size > "${output_file}" 2>&1 &
 
-    # Monitor the output
-    tail -f "${output_file}" | while read -r line; do
-        echo "$line"
-        if [[ "$line" == *"Finished. Exiting."* ]]; then
-            break
-        fi
-    done
-
-    git pull
-    git add --all
-    git commit -m "push ${output_file}"
-    git push
-
-    echo "-------"
+# Monitor the output
+tail -f "${output_file}" | while read -r line; do
+    echo "$line"
+    if [[ "$line" == *"Finished. Exiting."* ]]; then
+        break
+    fi
 done
+
+git pull
+git add --all
+git commit -m "push ${output_file}"
+git push
+
+echo "-------"
 
 echo "Benchmarking complete."
